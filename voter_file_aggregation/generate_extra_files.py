19d555f9bbd811c321ca3691ff5b41c04f9d3061
Kenny Joseph <kennyjoseph@Kennys-MacBook-Pro.local>
2017-05-25 14:02:20 -0400

    adding code to generate lists of individuals from outside state given in file

---
+++ voter_file_aggregation/generate_extra_files.py
@@ -0,0 +1,23 @@
+import io
+
+states = ['WY', 'WV', 'WI', 'WA', 'VT', 'VA', 'UT', 'TX', 'TN', 'SD', 'SC', 
+          'RI', 'PA', 'OR', 'OK', 'OH', 'NY', 'NV', 'NM', 'NJ', 'NH', 'NE',
+          'ND', 'NC', 'MT', 'MS', 'MO', 'MN', 'MI', 'ME', 'MD', 'MA', 'LA', 
+          'KY', 'KS', 'IN', 'IL', 'ID', 'IA', 'HI', 'GA', 'FL', 'DE', 'DC', 
+          'CT', 'CO', 'AZ', 'AR', 'AL', 'AK']
+
+state_map = {s : io.open(s+"_extra.tsv","w") for s in states}
+
+for fil in glob.glob("/net/data/twitter-voters/voter-data/ts_cleaned/*")
+    infil = io.open(fil)
+    print infil.readline()
+    for i, line in enumerate(infil):
+        x = line.split("\t")
+        if x[20] == "RI":
+            continue
+        elif x[20] in state_map:
+            state_map[x[20]].write(line)
+
+    infil.close()
+
+y = [x.close() for x in state_map.values()]
